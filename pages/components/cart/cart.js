74c6ed3e7f3d3b0f435ab539d8e6c78ddfd2dc81
328775856 <328775856@qq.com>
2018-03-26 22:49:12 +0800

    cart+pulldownfresh

--- pages/components/cart/cart.js
+++ pages/components/cart/cart.js
@@ -5,7 +5,106 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+      carts:[
+        {
+          pic: "http://mz.djmall.xmisp.cn/files/product/20161201/148058328876.jpg",
+          name: "日本资生堂洗颜",
+          price: 200,
+          isSelect: false,
+          // 数据设定
+          count: {
+            quantity: 2,
+            min: 1,
+            max: 20
+          },
+        },
+        {
+          pic: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058301941.jpg',
+          name: "倩碧焕妍活力精华露",
+          price: 340,
+          isSelect: false,
+          // 数据设定
+          count: {
+            quantity: 1,
+            min: 1,
+            max: 20
+          },
+        },
+        {
+          pic: 'http://mz.djmall.xmisp.cn/files/product/20161201/14805828016.jpg',
+          name: "特效润肤露",
+          price: 390,
+          isSelect: false,
+          // 数据设定
+          count: {
+            quantity: 3,
+            min: 1,
+            max: 20
+          },
+        },
+        {
+          pic: 'http://mz.djmall.xmisp.cn/files/product/20161201/148058228431.jpg',
+          name: "倩碧水嫩保湿精华面霜",
+          price: 490,
+          isSelect: false,
+          // 数据设定
+          count: {
+            quantity: 1,
+            min: 1,
+            max: 20
+          },
+        },
+        {
+          pic: 'http://mz.djmall.xmisp.cn/files/product/20161201/148057953326.jpg',
+          name: "兰蔻清莹柔肤爽肤水",
+          price: 289,
+          isSelect: false,
+          // 数据设定
+          count: {
+            quantity: 10,
+            min: 1,
+            max: 20
+          },
+        },
+        {
+          pic: "http://mz.djmall.xmisp.cn/files/product/20161201/148057921620_middle.jpg",
+          name: "LANCOME兰蔻小黑瓶精华",
+          price: 230,
+          isSelect: false,
+          // 数据设定
+          count: {
+            quantity: 1,
+            min: 1,
+            max: 20
+          },
+        },
+    ],
+    cur: 0,
+  },
+  isAllchecked:function(){
+    if (this.data.carts.length===6){
+      console.log(1234)
+    }
+  },
+  add:function(e){
+    this.setData({
+      quantity: quantity++
+    })
+    console.log(this.data.quantity)
+    //this.quantity++;
+  },
+  minus:function(){
+    this.quantity--;
+  },
+  onPullDownRefresh: function () {
+    wx.showNavigationBarLoading() //在标题栏中显示加载
+
+    //模拟加载
+    setTimeout(function () {
+      // complete
+      wx.hideNavigationBarLoading() //完成停止加载
+      wx.stopPullDownRefresh() //停止下拉刷新
+    }, 1500);
   },
 
   /**
